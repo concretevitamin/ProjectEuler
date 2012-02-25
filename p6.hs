8348b16f33508dbfc6d555acbe5a259c8211d8d2
Zongheng Yang <concretevitamin@gmail.com>
2012-02-25 18:59:36 +0800

    tricky composition

--- p6.hs
+++ p6.hs
@@ -1,3 +1,9 @@
-sum1 = sum [x^2 | x <- [1..100]]
-sum2 = (sum [x | x <- [1..100]])^2
-ans = abs (sum1 - sum2)
+--sum1 = sum [x^2 | x <- [1..100]]
+--sum2 = (sum [x | x <- [1..100]])^2
+--ans = abs (sum1 - sum2)
+
+--NOTE: the extra pair of parenthesis around the last term is needed, as (-) A B^2 actually means (A-B)^2.
+--ans = abs $ (-) (sum . map (^2) $ [1..100]) ((sum [1..100])^2)
+
+ans = abs . (-) (sum . map (^2) $ [1..100]) $ (sum [1..100])^2
+
