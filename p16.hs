16b26445681ba8a14cf518bb8f30af91fdb9cdb3
Zongheng Yang <concretevitamin@gmail.com>
2012-03-04 12:07:56 +0800

    can be shorter...

--- p16.hs
+++ p16.hs
@@ -1,8 +1,10 @@
 import Data.Char
+--
+--twoPower x
+--    | x == 1 = 2
+--    | odd x  = 2 * (twoPower (x `div` 2)) * (twoPower (x `div` 2))
+--    | even x = (twoPower (x `div` 2)) * (twoPower (x `div` 2))
+--
+--ans = foldl (\acc ch -> acc + digitToInt ch) 0 (show $ twoPower 1000)
 
-twoPower x
-    | x == 1 = 2
-    | odd x  = 2 * (twoPower (x `div` 2)) * (twoPower (x `div` 2))
-    | even x = (twoPower (x `div` 2)) * (twoPower (x `div` 2))
-
-ans = foldl (\acc ch -> acc + digitToInt ch) 0 (show $ twoPower 1000)
+ans = sum . map digitToInt . show $ 2^1000
