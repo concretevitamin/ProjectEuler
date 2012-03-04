b0ded4e63d9c85a386db21f8fa3a468a06724398
Zongheng Yang <concretevitamin@gmail.com>
2012-03-04 12:04:49 +0800

    laziness works like a charm.

---
+++ p16.hs
@@ -0,0 +1,8 @@
+import Data.Char
+
+twoPower x
+    | x == 1 = 2
+    | odd x  = 2 * (twoPower (x `div` 2)) * (twoPower (x `div` 2))
+    | even x = (twoPower (x `div` 2)) * (twoPower (x `div` 2))
+
+ans = foldl (\acc ch -> acc + digitToInt ch) 0 (show $ twoPower 1000)
